81b8992b14180abb230793c83ac795f941106f26
Lindsay Chapin <lnchapin@gmail.com>
2017-12-05 15:39:22 -0700

    working on home

--- capstone/app/views/Home.js
+++ capstone/app/views/Home.js
@@ -3,108 +3,83 @@ import {AppRegistry, Platform, StyleSheet, Text, View, TextInput, Button, AsyncS
 import {Actions} from 'react-native-router-flux'
 import Header from '../components/Header'
 
-
-let tasks = null;
-
 export default class Settings extends Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
-      userId: -1
+      userId: -1,
+      tasks: null
     }
   }
 
-  componentDidMount(){
+  componentDidMount() {
     console.log("in component did mount");
-    AsyncStorage.getItem('data')
-      .then(res =>
-        JSON.parse(res)
-      )
-      .then((user)=> {
-        this.setState({userId: user.user_id})
-        console.log("user.user_id", user.user_id)
-        console.log(this.state.userId)
-      }
-    )
-      .then( () => {
-      console.log("state at update", this.state);
-      fetch('https://fast-depths-36909.herokuapp.com/api/v1/tasks/user/'+`${this.state.userId}`)
-      .then(res => res.json())
-      .then(response => {
-        console.log(response);
-        tasks = response;
-        for (var i = 0; i < response.length; i++) {
-          console.log(response[i].task_name);
-        }
-      })
-      .catch(error => {
-        console.log("failure");
-        console.error(error);
-      })
-    })
+    AsyncStorage.getItem('data').then(res => JSON.parse(res)).then((user) => {
+      this.setState({userId: user.user_id})
+      console.log("user.user_id", user.user_id)
+      console.log(this.state.userId)
+    }).then(this.getUserTasks)
   }
 
   getUserTasks = () => {
     console.log("state at update", this.state);
-    fetch('https://fast-depths-36909.herokuapp.com/api/v1/tasks/user/'+`${this.state.userId}`)
-    .then(res => res.json())
-    .then(response => {
-      if(response.error){
-        alert(response.error)
-        console.log("if");
-      } else {
-        console.log("else");
-        console.log(response);
+    fetch('https://fast-depths-36909.herokuapp.com/api/v1/tasks/user/' + `${this.state.userId}`).then(res => res.json()).then(response => {
+      console.log(response);
+      this.setState({
+        tasks: response
+      })
+      console.log("state 31", this.state);
+      for (var i = 0; i < response.length; i++) {
+        console.log(response[i].task_name);
       }
-
-    })
-    .catch(error => {
+    }).catch(error => {
       console.log("failure");
       console.error(error);
     })
   }
 
-  asyncCheck = () => {
-    console.log(tasks);
-      AsyncStorage.getItem('data').then((res)=>console.log("getItem", res));
-      console.log(this.state.userId);
-      console.log("attempt at id from url", `${this.state.userId}`)
-      console.log('https://fast-depths-36909.herokuapp.com/api/v1/tasks/user/'+`${this.state.userId}`);
+  displayUserTasks = () => {
+    let arr = this.state.tasks
+    arr.map
+    for (var i = 0; i < arr.length; i++) {
+      console.log(arr[i].date)
+      console.log(arr[i].task_name)
+    }
   }
 
+  asyncCheck = () => {
+    console.log(tasks);
+    AsyncStorage.getItem('data').then((res) => console.log("getItem", res));
+    console.log(this.state.userId);
+    console.log("attempt at id from url", `${this.state.userId}`)
+    console.log('https://fast-depths-36909.herokuapp.com/api/v1/tasks/user/' + `${this.state.userId}`);
+  }
 
-  render (){
-    return(
-      <View>
-        <Header />
-        <View style={styles.myView}>
-          <Text style={styles.signUp}>Tasks</Text>
-          <View style={styles.buttonBack}>
-            <Button
-              onPress={this.getUserTasks}
-              title="Get Tasks"
-              accessibilityLabel="Get Tasks Button"
-            />
-          </View>
-          <View style={styles.buttonBack}>
-            <Button
-              onPress={this.asyncCheck}
-              title="Async Check"
-              accessibilityLabel="Async Check Button"
-            />
-          </View>
+  render() {
+    return (<View>
+      <Header/>
+      <View style={styles.myView}>
+        <Text style={styles.signUp}>Tasks</Text>
+        <View style={styles.buttonBack}>
+          <Button onPress={this.displayUserTasks} title="Display Tasks" accessibilityLabel="Get Tasks Button"/>
+        </View>
+        <View style={styles.buttonBack}>
+          <Button onPress={this.getUserTasks} title="Get Tasks" accessibilityLabel="Get Tasks Button"/>
+        </View>
+        <View style={styles.buttonBack}>
+          <Button onPress={this.asyncCheck} title="Async Check" accessibilityLabel="Async Check Button"/>
         </View>
       </View>
-    )
+    </View>)
   }
 }
 
 const styles = StyleSheet.create({
   myView: {
-    justifyContent:'center',
+    justifyContent: 'center',
     alignItems: 'center',
     padding: 5,
-    marginTop: 150,
+    marginTop: 150
   },
   signUp: {
     fontWeight: '700',
